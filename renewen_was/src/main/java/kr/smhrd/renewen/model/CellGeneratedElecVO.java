a3942d821a17ea08b4d8394302fa365677c863ae
cemujax <cemujax@gmail.com>
2024-03-04 10:44:18 +0900

    feat: Ardu => Was 발전셀 및 발전량 저장

---
+++ renewen_was/src/main/java/kr/smhrd/renewen/model/CellGeneratedElecVO.java
@@ -0,0 +1,20 @@
+package kr.smhrd.renewen.model;
+
+import lombok.Data;
+
+@Data
+public class CellGeneratedElecVO {
+
+    // 셀 발전량 식별번호 
+    private long cellGenNo;
+    // 발전셀 식별번호 
+    private long cellNo;
+    // 발전 전압(V) 
+    private int genVoltage;
+    // 발전 전류(A) 
+    private double genElecCurrent;
+    // 사용여부 
+    private String useYn;
+    // 측정 시간 
+	private String createdAt; 
+}
